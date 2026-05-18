201c53162c2db004e698dd1d526b1990b68ae7cb
Katriel <katrielmilena@gmail.com>
2026-05-18 10:42:47 -0300

    Implementação de um menu básico.

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-import parser
+from parser import *
 
-parser.parse_csv()
+opc: int = 0
+
+print("| Opções:")
+print("| 1 - Conversor de bases")
+print("| 2 - Calculadora de Máximos")
+print("| 3 - Modo Quiz")
+print("| 4 - Sair")
+print("~ ---------------------------------------------")
+
+while opc != 4:
+    opc = int(input("Insira uma das opções: "))
+    if (opc > 4) or (opc < 1):
+        print("\n| Opção inexistente! As opções são:")
+        print("| 1 - Conversor de bases")
+        print("| 2 - Calculadora de Máximos")
+        print("| 3 - Modo Quiz")
+        print("| 4 - Sair")
+        print("~ ---------------------------------------------")
+
+    match opc:
+        case 1:
+            print("Em desenvolvimento\n")
+        case 2:
+            print("Em desenvolvimento\n")
+        case 3:
+            print("Em desenvolvimento\n")
+
+        case 4:
+            print("Saindo...")
+
+
+
