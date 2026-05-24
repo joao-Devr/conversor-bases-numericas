a07327bec41623d6569595147e86bf98f9fc842a
joao-Devr <jpcrodrigues75@gmail.com>
2026-05-23 21:10:18 -0300

    cobrindo todos os casos de entrada no formatador, add casos de teste no entrada.csv

--- formatador.py
+++ formatador.py
@@ -3,35 +3,45 @@ from conversor import *
 def resposta_terminal(base_origem: int, base_destino: int, valor: str) -> None:
 
     if base_origem == 10:
+        
         resposta = decimal_universal(valor, base_destino)
 
-    if base_destino ==10:
-        resposta = universal_decimal(valor, int(base_origem))
-    
-    elif base_origem == 16 or base_origem == 8:
-        if base_destino == 2:
-            resposta = ho_binario(valor, base_origem)
-        else:
-            resposta = ho(valor, base_origem, base_destino)
-    
-    if base_origem == 2:
+    elif base_destino == 10:
+
+        resposta = universal_decimal(valor, base_origem)
+
+    elif base_origem == 2 and (base_destino == 16 or base_destino == 8):
+
         resposta = binario_ho(valor, base_destino)
 
-    
+    elif (base_origem == 16 or base_origem == 8) and base_destino == 2:
+
+        resposta = ho_binario(valor, base_origem)
+
+    else:
+        resposta = ho(valor, base_origem, base_destino)
 
     print(f"Resposta final: {resposta} \n \n")
 
 def resposta_csv( valor: str, base_origem: str, base_destino: str) -> None:
     import csv
+    base_origem = int(base_origem)
+    base_destino = int(base_destino)
 
-    if int(base_origem) == 10:
-            resposta = decimal_universal(valor, int(base_destino))
-            
-    if int(base_destino)==10:
-        resposta = universal_decimal(valor, int(base_origem))
-    
-    if (int(base_origem) == 2 and int(base_destino) == 16) or (int(base_origem) == 16 and int(base_destino) == 2):
-        resposta = binario_hexa(valor, int(base_origem))
+    if base_origem == 10:
+        resposta = decimal_universal(valor, base_destino)
+
+    elif base_destino == 10:
+        resposta = universal_decimal(valor, base_origem)
+
+    elif base_origem == 2 and (base_destino == 16 or base_destino == 8):
+        resposta = binario_ho(valor, base_destino)
+
+    elif (base_origem == 16 or base_origem == 8) and base_destino == 2:
+        resposta = ho_binario(valor, base_origem)
+
+    else:
+        resposta = ho(valor, base_origem, base_destino)
 
     with open("respostas.csv", "a", newline="", encoding="utf-8") as arquivo:
 
