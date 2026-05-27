abd235cf24e51c93826200658d292764e00564b8
Katriel <katrielmilena@gmail.com>
2026-05-27 09:24:00 -0300

    Implemetacao de perguntas de numeros com ',', remocao das respostas imprimirem para o teste, remocao do teste em si, além disso, foi adicionado um sistema de pontuacao

--- quiz_core.py
+++ quiz_core.py
@@ -2,13 +2,28 @@ import random
 from conversor import *
 from formatador import resposta_terminal
 
-def gera_numero(tamanho: int, permitidos) -> str:
+def gera_numero(tamanho: int, permitidos, depois_virgula: bool) -> str:
     
     numero_pergunta: str = ''
 
     for i in range(0, tamanho):
         numero_pergunta = random.choice(permitidos) + numero_pergunta 
+    
+    indice_remover: int = 0
+    inicio = 1
+    fim = len(numero_pergunta)
 
+    if depois_virgula == True:
+        indice_remover = -1
+        inicio = 0
+        fim = -1
+
+    while numero_pergunta[indice_remover] == '0':
+        numero_pergunta = numero_pergunta[inicio:fim]
+        if numero_pergunta == '':
+            numero_pergunta = gera_numero(tamanho, permitidos, depois_virgula)
+
+    
     return numero_pergunta
 
 def quiz_questao(dificuldade: int, base_anterior: int):
@@ -27,6 +42,7 @@ def quiz_questao(dificuldade: int, base_anterior: int):
 
     # Não precisa de elif, porque ele vai diminuindo conforme a dificuldade aumenta
     tentativas: int = 2
+
     if dificuldade == 4:
         tentativas = tentativas - 1
     if dificuldade == 5:
@@ -43,22 +59,21 @@ def quiz_questao(dificuldade: int, base_anterior: int):
     base_destino: int = 0 
     base_destino: int = random.choice(bases)
     
-    trecho_permitido: int = base_origem
-    if base_origem == 16 and dificuldade <= 2:
-        trecho_permitido = 10
-    
     permitidos = tabela_hexadecimal[:2]
-    numero_pergunta: str = gera_numero(tamanho, permitidos)   
-    while numero_pergunta[0] == '0':
-        numero_pergunta = numero_pergunta[1:]
-        if numero_pergunta == '':
-            numero_pergunta = gera_numero(tamanho, permitidos)
+    numero_pergunta_antes_virgula: str = gera_numero(tamanho, permitidos, False)
+    numero_pergunta_depois_virgula: str = ''
+    
+    if dificuldade >= 4:
+        numero_pergunta_depois_virgula = '.' + gera_numero(4, permitidos, True)
+    
+    numero_pergunta: str = numero_pergunta_antes_virgula + numero_pergunta_depois_virgula
+
     if base_origem != 2:
         numero_pergunta = resposta_terminal(2, base_origem, numero_pergunta, False)
 
+
     resposta_certa: str = resposta_terminal(base_origem, base_destino, numero_pergunta, False)
     acerto_erro: int = 0
-    print (resposta_certa)
     print(f"Converta o número {numero_pergunta} na base {base_origem}, para base {base_destino}.")
     resposta: str= input("Insira a resposta: ")
     while resposta != resposta_certa and tentativas != 0: 
@@ -71,9 +86,16 @@ def quiz_questao(dificuldade: int, base_anterior: int):
 
     return base_origem, acerto_erro
 
-base_anterior = 0
-acerto_erro = [0, 0, 0, 0, 0]
-for i in range(0, 5):
-    base_anterior, acerto_erro[i] = quiz_questao(3, base_anterior)
+def quiz_start(dificuldade: int, quantidade: int) -> int:
+    pontuacao: int = 0
+    acertos_erros: list[int] = []
 
-print(acerto_erro)
+    for i in range(0, quantidade):
+        base_anterior = 0
+        acerto_erro: int = 0
+        base_anterior, acerto_erro = quiz_questao(dificuldade, base_anterior)
+        acertos_erros.append(acerto_erro)
+
+    pontuacao = sum(acertos_erros)
+
+    return pontuacao
